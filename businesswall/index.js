821fbd6b63c847a3340fa948c4098ef506c480c9
Cactus <157395485+C4ctus72@users.noreply.github.com>
2025-01-03 20:20:20 +0100

    modified:   businesswall/index.js

--- businesswall/index.js
+++ businesswall/index.js
@@ -1,24 +1,22 @@
 console.log("Initializing...");
 console.log("Ready!");
 
+function TimeGrabber(Time){
+    const Hours = Time.getHours().toString().padStart(2, 0);
+    const Minutes = Time.getMinutes().toString().padStart(2, 0);
+    const Seconds = Time.getSeconds().toString().padStart(2, 0);
+    const DisplayTime = Hours + ":" + Minutes + ":" + Seconds;
+    return DisplayTime;
+};
 function GetGermanyTime() {
     const TimeInGerman = new Date;
-    const HoursInGermany = TimeInGerman.getHours().toString().padStart(2, 0);
-    const MinutesInGermany = TimeInGerman.getMinutes().toString().padStart(2, 0);
-    const SecondsInGermany = TimeInGerman.getSeconds().toString().padStart(2, 0);
-    Time = HoursInGermany + ":" + MinutesInGermany + ":" + SecondsInGermany;
-    document.getElementById("CurrentTimeGermany").textContent = Time;
+    document.getElementById("CurrentTimeGermany").textContent = TimeGrabber(TimeInGerman);
 };
 function GetJapanTime(){
     const TimeInJapan = new Date;
-    const HoursInJapan = TimeInJapan.getHours().toString().padStart(2, 0);
-    const MinutesInJapan = TimeInJapan.getMinutes().toString().padStart(2, 0);
-    const SecondsInJapan = TimeInJapan.getSeconds().toString().padStart(2, 0);
-    Time = HoursInJapan + ":" + MinutesInJapan + ":" + SecondsInJapan;
-    document.getElementById("CurrentTimeJapan").textContent = Time;
+    document.getElementById("CurrentTimeJapan").textContent = TimeGrabber(TimeInJapan);
 };
-
-
 GetGermanyTime();
 GetJapanTime();
-setInterval(GetGermanyTime, GetJapanTime, 1000);
+setInterval(GetGermanyTime, 1000);
+setInterval(GetJapanTime, 1000);
