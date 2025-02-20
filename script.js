a50d5699692237fb4ac951c184796f8a31733588
Hactus <jo04_ho@icloud.com>
2025-02-20 16:46:44 +0100

    modified:   index.html 	modified:   script.js 	new file:   style/index.css

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-const a = document.querySelector('a')
-let changethis = document.getElementsByClassName('pointer')
+// const a = document.querySelector('a')
+// let changethis = document.getElementsByClassName('pointer')
 
-a.addEventListener('mouseover', () => {
-    changethis.innerText = ">"
-})
+// a.addEventListener('mouseover', () => {
+//     changethis.innerText = ">"
+// })
 
-let main = document.getElementById('cool-people');
-let openclose = document.getElementById('open-close');
-main.style.display = "none"
+let cool_people = document.getElementById("cool-people");
+let openclose_cp = document.getElementById("open-close");
+cool_people.style.display = "none";
 
-function ddm(){
-    switch (main.style.display) {
-        case "none":
-            main.style.display = "block"
-            openclose.innerText = "[v]"
-            break;
-        case "block":
-            main.style.display = "none"
-            openclose.innerText = "[>]"
-
-            break;
-        default:
-            break;
-    }
+function ddm(x) {
+  switch (x.style.display) {
+    case "none":
+      x.style.display = "block";
+      openclose_cp.innerText = "[v]";
+      break;
+    case "block":
+      x.style.display = "none";
+      openclose_cp.innerText = "[>]";
+      break;
+    default:
+      break;
+  }
 }
