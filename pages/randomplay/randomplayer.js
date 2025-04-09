571ee903afc59b490eba2d8853780eedb2d9f18f
Hactus <jo04_ho@icloud.com>
2025-04-09 21:15:23 +0200

    modified:   index.html 	new file:   pages/randomplay/randomplayer.css 	new file:   pages/randomplay/randomplayer.html 	new file:   pages/randomplay/randomplayer.js

---
+++ pages/randomplay/randomplayer.js
@@ -0,0 +1,10 @@
+// https://splatoonwiki.org/wiki/Octo_Expansion#Music
+// Albumcovers
+const dedf1sh = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/0/01/S2_Band_Dedf1sh.jpg/604px-S2_Band_Dedf1sh.jpg"
+const turquise_october = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/6/6c/S2_Band_Turquoise_October.jpg/610px-S2_Band_Turquoise_October.jpg"
+const off_the_hook = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/c6/S2_Band_Off_the_Hook.jpg/623px-S2_Band_Off_the_Hook.jpg"
+const squid_sisters = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/c8/S_Band_Squid_Sisters.jpg/605px-S_Band_Squid_Sisters.jpg"
+
+function SongPicker(){
+
+}
