04f433a0b0630eac4be079816becccc4da933e04
Hactus <jo04_ho@icloud.com>
2025-04-09 18:00:50 +0200

    modified:   pages/timezones/office.html 	modified:   pages/timezones/office.js

--- pages/timezones/office.js
+++ pages/timezones/office.js
@@ -14,7 +14,10 @@ let DateVar = new Date();
 const timezonede = "Europe/Berlin"
 const timezonejp = "Asia/Tokyo"
 const timezoneca = "America/Toronto"
+const timezoneNY = "America/New_York"
 const timezoneLA = "America/Los_Angeles"
+const timezonefr = "Europe/Paris"
+const timezonech = "Asia/Bejing"
 //Print 2 screen
 function print(HtmlElement, timezone){
     document.getElementById(HtmlElement).textContent = DateVar.toLocaleTimeString(undefined, { timeZone: timezone, ...options, })
@@ -24,6 +27,9 @@ print("CurrentTimeGermany", timezonede)
 print("CurrentTimeJapan", timezonejp)
 print("CurrentTimeCanada", timezoneca)
 print("CurrentTimeLA", timezoneLA)
+print("CurrentTimeParis", timezonefr)
+print("CurrentTimeBejing", timezoneca)
+print("CurrentTimeNYC", timezoneNY)
 
 setInterval(() => {
     DateVar = new Date()
@@ -31,4 +37,7 @@ setInterval(() => {
     print("CurrentTimeJapan", timezonejp)
     print("CurrentTimeCanada", timezoneca)
     print("CurrentTimeLA", timezoneLA)
-},  1000);
+    print("CurrentTimeParis", timezonefr)
+    print("CurrentTimeBejing", timezoneca)
+    print("CurrentTimeNYC", timezoneNY)
+  },  1000);
