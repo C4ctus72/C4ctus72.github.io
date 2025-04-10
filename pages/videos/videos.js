3e400febeae5d560ea8d28e4dd3095dd34208265
Hactus <jo04_ho@icloud.com>
2025-04-10 21:38:21 +0200

    modified:   pages/videos/videos.css 	modified:   pages/videos/videos.html 	modified:   pages/videos/videos.js

--- pages/videos/videos.js
+++ pages/videos/videos.js
@@ -1,14 +1,34 @@
-let nonvisible_player = document.getElementById('video_player')
-nonvisible_player.style.display = "none"
+const jit_button = document.getElementById("jit_button")
+const shade_button = document.getElementById("shade_button")
+let jit_player = document.getElementById("jit_player")
+let shade_player = document.getElementById("shade_player")
+jit_player.style.display = "none"
+shade_player.style.display = "none"
 
-function toggle_visible(){
-    switch (nonvisible_player.style.display) {
+
+function toggle_jit(){
+    switch (jit_player.style.display) {
         case "none":
-            nonvisible_player.style.display = "block"
+            jit_player.style.display = "block"
             break;
+
         case "block":
-            nonvisible_player.style.display = "none"
-            break
+            jit_player.style.display = "none"
+
+        default:
+            break;
+    }
+}
+
+function toggle_shade(){
+    switch (shade_player.style.display) {
+        case "none":
+            shade_player.style.display = "block"
+            break;
+
+        case "block":
+            shade_player.style.display = "none"
+
         default:
             break;
     }
