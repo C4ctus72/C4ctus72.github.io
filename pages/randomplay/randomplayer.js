0559f33772684a1930d7fff6151a752df3382a75
Hactus <jo04_ho@icloud.com>
2025-04-11 17:30:51 +0200

    modified:   pages/randomplay/randomplayer.html modified:   pages/randomplay/randomplayer.js

--- pages/randomplay/randomplayer.js
+++ pages/randomplay/randomplayer.js
@@ -13,14 +13,21 @@ function toggle_message(x) {
     }
 }
 
-// https://splatoonwiki.org/wiki/Octo_Expansion#Music
 // Albumcovers
+// https://splatoonwiki.org/wiki/Octo_Expansion#Music
 const dedf1sh = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/0/01/S2_Band_Dedf1sh.jpg/604px-S2_Band_Dedf1sh.jpg"
 const turquise_october = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/6/6c/S2_Band_Turquoise_October.jpg/610px-S2_Band_Turquoise_October.jpg"
 const off_the_hook = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/c6/S2_Band_Off_the_Hook.jpg/623px-S2_Band_Off_the_Hook.jpg"
 const squid_sisters = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/c8/S_Band_Squid_Sisters.jpg/605px-S_Band_Squid_Sisters.jpg"
 
-let Albumcover = document.getElementById("AlbumCover")
+//dedf1sh songs
+let songs_dedf1sh = new Array()
+songs_dedf1sh = ["#0 shell", "#1 progress", "#2 ripped", "#4 dunno", "#5 thirsty", "#6 frisk", "#8 regret", "#9 party", "#11 above", "#12 awake", "#13 shade", "#14 crush", "#16 salty", "#19 bless", "Splattack! (Octo)"]
+
+let AlbumCover = document.getElementById("AlbumCover")
+
+const covers = [dedf1sh, turquise_october, off_the_hook, squid_sisters]
+
 
 function SongPicker(){
     
