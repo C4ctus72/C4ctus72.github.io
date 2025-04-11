b794268615b6c99a63f0fd21db363bae44d84dd9
Hactus <jo04_ho@icloud.com>
2025-04-11 17:42:16 +0200

    modified:   pages/randomplay/randomplayer.js

--- pages/randomplay/randomplayer.js
+++ pages/randomplay/randomplayer.js
@@ -19,26 +19,19 @@ const dedf1sh = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/0/01/S2_Ban
 const turquise_october = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/6/6c/S2_Band_Turquoise_October.jpg/610px-S2_Band_Turquoise_October.jpg"
 const off_the_hook = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/c6/S2_Band_Off_the_Hook.jpg/623px-S2_Band_Off_the_Hook.jpg"
 const squid_sisters = "https://cdn.wikimg.net/en/splatoonwiki/images/thumb/c/c8/S_Band_Squid_Sisters.jpg/605px-S_Band_Squid_Sisters.jpg"
-
-//dedf1sh songs
-let songs_dedf1sh = new Array()
-songs_dedf1sh = ["#0 shell", "#1 progress", "#2 ripped", "#4 dunno", "#5 thirsty", "#6 frisk", "#8 regret", "#9 party", "#11 above", "#12 awake", "#13 shade", "#14 crush", "#16 salty", "#19 bless", "Splattack! (Octo)"]
-
 let AlbumCover = document.getElementById("AlbumCover")
-
 const covers = [dedf1sh, turquise_october, off_the_hook, squid_sisters]
 
+// Song titles
+// https://splatoonwiki.org/wiki/Octo_Expansion#Music
+let songs_dedf1sh, songs_off_the_hook, songs_turquoise_october, songs_squid_sisters = new Array()
+songs_dedf1sh = ["#0 shell", "#1 progress", "#2 ripped", "#4 dunno", "#5 thirsty", "#6 frisk", "#8 regret", "#9 party", "#11 above", "#12 awake", "#13 shade", "#14 crush", "#16 salty", "#19 bless", "Splattack! (Octo)"]
+songs_off_the_hook = ["Nasty Majesty", "Shark Bytes", "Here Comes Ultimate Doom", "Fly Octo Fly ~ Ebb & Flow (Octo)", "Into the Light", "Ebb&FlowDemo.frsh (Sketch)", "#$@%* Dudes Be #$@%* Sleepin' (Live)"]
+songs_turquoise_october = ["Octoling Rendezvous"]
+songs_squid_sisters = ["Calamari Inkantation (Inner Agent 3)"]
 
-function SongPicker(){
-    
-}
-if(window.open()){
-    SongPicker()
-}
-
-function SetAlbumCover(){
-
-    Albumcover.src = dedf1sh;
+function SetAlbumCover(Cover){
+    Albumcover.src = Cover;
 }
 
 SetAlbumCover()
