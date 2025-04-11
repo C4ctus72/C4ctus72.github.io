2539608e193c1a3728e8695b1bdc68ef495ca202
Hactus <jo04_ho@icloud.com>
2025-04-11 14:02:00 +0200

    modified:   index.html 	modified:   pages/others/others.html 	modified:   pages/randomplay/randomplayer.html 	modified:   pages/template.html 	modified:   pages/videos/videos.html 	modified:   pages/videos/videos.js

--- pages/videos/videos.js
+++ pages/videos/videos.js
@@ -1,35 +1,21 @@
 const jit_button = document.getElementById("jit_button")
 const shade_button = document.getElementById("shade_button")
+
 let jit_player = document.getElementById("jit_player")
 let shade_player = document.getElementById("shade_player")
+
 jit_player.style.display = "none"
 shade_player.style.display = "none"
 
-
-function toggle_jit(){
-    switch (jit_player.style.display) {
+function toggle_player(x){
+    switch(x.style.display){
         case "none":
-            jit_player.style.display = "block"
-            break;
-
+            x.style.display = "block"
+            break
         case "block":
-            jit_player.style.display = "none"
-
+            x.style.display = "none"
+            break
         default:
-            break;
-    }
-}
-
-function toggle_shade(){
-    switch (shade_player.style.display) {
-        case "none":
-            shade_player.style.display = "block"
-            break;
-
-        case "block":
-            shade_player.style.display = "none"
-
-        default:
-            break;
+            break
     }
 }
